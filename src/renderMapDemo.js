8fcfc46cd146eb4f913d98efaac8e489f288cecc
Karan Sequeira <Karan@KMBP.local>
2016-09-17 12:47:45 -0600

    Incorporated revised JSON structure of response from api/sites.php...

--- src/renderMapDemo.js
+++ src/renderMapDemo.js
@@ -3,7 +3,7 @@ Test URL: http://localhost/~karan/SGD01/
 */
 
 var RenderMapDemo = function() {
-	this.testURL = "data/sites.json";	//http://wateriso.utah.edu/api/sites.php";
+	this.testURL = "http://wateriso.utah.edu/api/sites.php";//"data/sites_02.json";	//"http://wateriso.utah.edu/api/sites.php";
 	this.jsonData = null;
 
 	this.map = null;
@@ -18,15 +18,16 @@ RenderMapDemo.prototype.initVectorSource = function() {
 
 	// for each point that must be plotted, create a feature
 	var numSitesPlotted = 0;
-	for (var i = 0; i < this.jsonData.length; ++i) {
-		if (isNaN(parseFloat(this.jsonData[i].Latitude)) || isNaN(parseFloat(this.jsonData[i].Longitude)))
+	for (var i = 0; i < this.jsonData.sites.length; ++i) {
+		var site = this.jsonData.sites[i];
+		if (isNaN(parseFloat(site.Latitude)) || isNaN(parseFloat(site.Longitude)))
 		{
-			console.log("Found undefined Lat/Lon for site:" + this.jsonData[i].Site_ID + " Lat:" + this.jsonData[i].Latitude + " Lon:" + this.jsonData[i].Longitude);
+			console.log("Found undefined Lat/Lon for site:" + site.Site_ID + " Lat:" + site.Latitude + " Lon:" + site.Longitude);
 			continue;
 		}
 
 		var point = new ol.geom.Point(null);
-		point.setCoordinates(ol.proj.fromLonLat([parseFloat(this.jsonData[i].Longitude), parseFloat(this.jsonData[i].Latitude)]));
+		point.setCoordinates(ol.proj.fromLonLat([parseFloat(site.Longitude), parseFloat(site.Latitude)]));
 
 		featureCollection.push(new ol.Feature({
 			geometry: point
@@ -81,8 +82,13 @@ RenderMapDemo.prototype.fetchJSON = function() {
 			withCredentials: false
 		},
 		success: function(data) {
-			console.log("Response received...");
-			renderMapDemo.onJSONReceived(data);
+			if (data.status.Code == 200) {
+				console.log("Response received...");
+				renderMapDemo.onJSONReceived(data);
+			}
+			else {
+				console.log("Received response with error:" + data.status.Code + " and message:" + data.status.Message);
+			}
 		},
 		error: function() {
 			console.log("Something went wrong...");
